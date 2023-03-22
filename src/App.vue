4b725f8443e53ef8861693d003e159780420bcc6
JosCha94 <72150891+JosCha94@users.noreply.github.com>
2023-03-21 23:59:35 -0500

    si

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,72 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import Card from './components/Card.vue';
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Card
+  },
 
+  data() {
+    return {
+      canciones: [],
+      texto: "",
+      tipo: "",
+      audio: "",
+      audio2: ""
+    }
+  },
+
+  methods: {
+    async buscarXCancion() {
+      try {
+        const datos = await fetch("https://api.deezer.com/search/track?q=" + this.texto)
+        const respuesta = await datos.json();
+        this.canciones = respuesta.data;
+        this.tipo = "Canciones";
+
+      } catch (error) {
+        this.error = error.message;
+      }
+    },
+
+    async buscarXAlbum() {
+      try {
+        const datos = await fetch("https://api.deezer.com/search/album?q=" + this.texto)
+        const respuesta = await datos.json();
+        this.canciones = respuesta.data;
+        this.tipo = "Álbums";
+
+      } catch (error) {
+        this.error = error.message;
+      }
+    },
+
+    async buscarXArtista() {
+      try {
+        const datos = await fetch("https://api.deezer.com/search/artist?q=" + this.texto)
+        const respuesta = await datos.json();
+        this.canciones = respuesta.data;
+        this.tipo = "Artistas";
+
+      } catch (error) {
+        this.error = error.message;
+      }
+    },
+
+    async agregarAudio() {
+      try {
+        this.audio = this.audio2;
+      } catch (error) {
+        this.error = error.message;
+      }
+    }
+  }
+}
+</script>
 <template>
-  <div class="row">
+  <div class="row m-0">
     <div class="col-12 col-md-3" style="background-color: #662323;">
       <div class="container my-5">
         <img src="./assets/imgs/foxbel-music.png" alt="" class="img-fluid mx-auto d-bloc">
@@ -39,73 +105,35 @@
             <h6>Usuario</h6>
           </div>
           <div class="col-12">
-            <h3>Cosa</h3>
-            <!-- <p>{{ texto }}</p> -->
+
+
           </div>
-          <div class="col-12">
-            <div v-for="cancion in canciones" :key="cancion.id">
-              <!-- <h3>{{ cancion.title }}</h3> -->
+          <div class="col-12" id="result">
+            <h3>{{ tipo }}</h3>
+            <h3>{{ audio }}</h3>
+            <div class="gap-3" id="result">
+
+              <div v-for="cancion in canciones" :key="cancion.id">
+                
+                  <Card :titulo="cancion.title" :artista="cancion.artist.name" :imge="cancion.cover" />
+                <!-- <input type="hidden" :value="cancion.previe">
+<button @click="agregarAudio">Escuchar</button> -->
+                <!-- <Card :titulo="cancion.name" :artista="cancion.name" :imge="cancion.picture" /> -->
+              </div>
+
             </div>
           </div>
         </div>
       </div>
     </div>
   </div>
-  <div class="row bg-danger p-5"></div>
+  <div class="row bg-danger p-4 fixed-bottom">
+    <audio controls>
+      <source :src="audio" type="audio/mpeg">
+    </audio>
+
+  </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
-
-  data() {
-    return {
-      canciones: [],
-      texto: ""
-    }
-  },
-
-  methods: {
-    async buscarXCancion() {
-      try {
-        const datos = await fetch("https://api.deezer.com/search/track?q=" + this.texto)
-        const respuesta = await datos.json();
-        this.canciones = respuesta.data;
-
-      } catch (error) {
-        this.error = error.message;
-      }
-    },
-
-    async buscarXAlbum() {
-      try {
-        const datos = await fetch("https://api.deezer.com/search/album?q=" + this.texto)
-        const respuesta = await datos.json();
-        this.canciones = respuesta.data;
-
-      } catch (error) {
-        this.error = error.message;
-      }
-    },
-
-    async buscarXArtista() {
-      try {
-        const datos = await fetch("https://api.deezer.com/search/artist?q=" + this.texto)
-        const respuesta = await datos.json();
-        this.canciones = respuesta.data;
-
-      } catch (error) {
-        this.error = error.message;
-      }
-    }
-  }
-}
-</script>
-
 
 <style>
 ul li a {
@@ -123,5 +151,10 @@ li {
 
 li:hover {
   color: #eb6060;
+}
+
+#result {
+  display: flex;
+  flex-wrap: wrap;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="card" style="width: 10rem;">
+        <img :src=" imge " class="card-img-top" :alt="titulo">
+        <div class="card-body">
+            <h5 class="card-title">{{ titulo }}</h5>
+            <p class="card-text"><small class="text-muted">{{ artista }}</small></p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Card',
+    props: {
+        titulo: String,
+        artista: String,
+        imge: String,
+    }
+}
+</script>
+
+    <!-- <div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img :src=" imge " class="img-fluid rounded-start" :alt="titulo">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">{{ titulo }}</h5>
+        <p class="card-text">{{ artista }}</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div> -->
