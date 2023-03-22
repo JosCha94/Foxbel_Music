642e1a515c31ed4e6c3fecf9838170d3d077e4fa
JosCha94 <72150891+JosCha94@users.noreply.github.com>
2023-03-22 18:56:23 -0500

    cambios

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-import Card from './components/Card.vue';
+import Card from "./components/Card.vue";
+import Audio from "./components/Audio.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld,
-    Card
+    Card,
+    Audio,
   },
 
   data() {
@@ -13,19 +13,17 @@ export default {
       canciones: [],
       texto: "",
       tipo: "",
-      audio: "",
-      audio2: ""
-    }
+      cancion: "",
+    };
   },
 
   methods: {
     async buscarXCancion() {
       try {
-        const datos = await fetch("https://api.deezer.com/search/track?q=" + this.texto)
+        const datos = await fetch("https://api.deezer.com/search/track?q=" + this.texto);
         const respuesta = await datos.json();
         this.canciones = respuesta.data;
         this.tipo = "Canciones";
-
       } catch (error) {
         this.error = error.message;
       }
@@ -33,11 +31,10 @@ export default {
 
     async buscarXAlbum() {
       try {
-        const datos = await fetch("https://api.deezer.com/search/album?q=" + this.texto)
+        const datos = await fetch("https://api.deezer.com/search/album?q=" + this.texto);
         const respuesta = await datos.json();
         this.canciones = respuesta.data;
         this.tipo = "Álbums";
-
       } catch (error) {
         this.error = error.message;
       }
@@ -45,11 +42,10 @@ export default {
 
     async buscarXArtista() {
       try {
-        const datos = await fetch("https://api.deezer.com/search/artist?q=" + this.texto)
+        const datos = await fetch("https://api.deezer.com/search/artist?q=" + this.texto);
         const respuesta = await datos.json();
         this.canciones = respuesta.data;
         this.tipo = "Artistas";
-
       } catch (error) {
         this.error = error.message;
       }
@@ -61,18 +57,23 @@ export default {
       } catch (error) {
         this.error = error.message;
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
 <template>
-  <div class="row m-0">
-    <div class="col-12 col-md-3" style="background-color: #662323;">
+  <div class="row m-0" style="height: 100vh">
+    <!-- MENU - BARRA LATERAL -->
+    <div class="col-12 col-md-3" style="background-color: #662323">
       <div class="container my-5">
-        <img src="./assets/imgs/foxbel-music.png" alt="" class="img-fluid mx-auto d-bloc">
+        <img
+          src="./assets/imgs/foxbel-music.png"
+          alt=""
+          class="img-fluid mx-auto d-bloc"
+        />
         <div class="mt-5 ms-4">
-          <h4 class="text-light">Mi Librería</h4>
-          <ul style="list-style: none;">
+          <h4 class="text-light">Categorias</h4>
+          <ul style="list-style: none">
             <li>Recientes</li>
             <li><a href="#" @click="buscarXArtista">Artistas</a></li>
             <li><a href="#" @click="buscarXAlbum">Álbums</a></li>
@@ -81,61 +82,110 @@ export default {
           </ul>
 
           <h4 class="text-light">Playlist</h4>
-          <ul style="list-style: none;">
+          <ul style="list-style: none">
             <li>Metal</li>
             <li>Para Bailar</li>
             <li>Rock 90s</li>
             <li>Baladas</li>
           </ul>
         </div>
-
       </div>
-      <!-- <BarraLateral /> -->
     </div>
-    <div class="col-12 col-md-9 bg-info">
-      <div class="container ">
+    <!-- PRINCIPAL -->
+    <div class="col-12 col-md-9" style="background-color: #f7fff2">
+      <div class="container">
         <div class="row mx-1 mx-md-3 my-4">
           <div class="col-7">
             <div class="input-group mb-3">
-              <input type="text" class="form-control" placeholder="Buscar" v-model="texto">
-              <span class="input-group-text" id="basic-addon1">@</span>
+              <input
+                type="text"
+                class="form-control"
+                placeholder="Buscar"
+                v-model="texto"
+                style="border-top-left-radius: 60px 60px; border-bottom-left-radius: 60px 60px;"
+              />
+              <span class="input-group-text bg-body" id="basic-addon1" style="border-top-right-radius: 60px 60px; border-bottom-right-radius: 60px 60px;"
+                ><i class="fa-solid fa-magnifying-glass" style="color: #f2f2f2"></i
+              ></span>
             </div>
           </div>
-          <div class="col-5 text-end ">
-            <h6>Usuario</h6>
+          <div class="col-5 text-end">
+            <h6><i class="fa-solid fa-user" style="color: #eb5757"></i> Usuario</h6>
           </div>
+          <div class="col-12"></div>
           <div class="col-12">
-
-
-          </div>
-          <div class="col-12" id="result">
             <h3>{{ tipo }}</h3>
-            <h3>{{ audio }}</h3>
-            <div class="gap-3" id="result">
-
-              <div v-for="cancion in canciones" :key="cancion.id">
-                
-                  <Card :titulo="cancion.title" :artista="cancion.artist.name" :imge="cancion.cover" />
-                <!-- <input type="hidden" :value="cancion.previe">
-<button @click="agregarAudio">Escuchar</button> -->
-                <!-- <Card :titulo="cancion.name" :artista="cancion.name" :imge="cancion.picture" /> -->
+            <div v-if="this.tipo === 'Álbums'">
+              <div class="gap-3" id="result">
+                <div v-for="cancion in canciones" :key="cancion.id">
+                  <Card
+                    :titulo="cancion.title"
+                    :artista="cancion.artist.name"
+                    :imge="cancion.cover_big"
+                    :audio="cancion.preview"
+                    :tipo="this.tipo"
+                    :tracks="cancion.nb_tracks"
+                  />
+                </div>
               </div>
-
+            </div>
+            <div v-else-if="this.tipo === 'Canciones'">
+              <div class="gap-3" id="result">
+                <div v-for="cancion in canciones" :key="cancion.id">
+                  <Card
+                    :titulo="cancion.title"
+                    :artista="cancion.artist.name"
+                    :imge="cancion.album.cover"
+                    :audio="cancion.preview"
+                    :tipo="this.tipo"
+                  />
+                </div>
+              </div>
+            </div>
+            <div v-else-if="this.tipo === 'Artistas'">
+              <div class="gap-3" id="result">
+                <div v-for="cancion in canciones" :key="cancion.id">
+                  <Card
+                    :titulo="cancion.name"
+                    :artista="cancion.name"
+                    :imge="cancion.picture_big"
+                    :audio="cancion.preview"
+                    :tipo="this.tipo"
+                    :albums="cancion.nb_album"
+                    :fans="cancion.nb_fan"
+                  />
+                </div>
+              </div>
+            </div>
+            <div v-else>
+              <div class="container text-center my-5">
+                <div style="margin-top: 20%">
+                  <h1 class="mb-4">Bienvenido a FOXBEL MUSIC</h1>
+                  <marquee direction="left">
+                    <p class="fw-5">
+                      Para disfrutar de nuestra buena musica, escribe en la barra de
+                      arriba
+                      <i class="fa-solid fa-arrow-up"></i> el nombre del artista, álbum o
+                      canción que deseas buscar y selecciona es la barra de la izquierda
+                      <i class="fa-solid fa-arrow-left"></i> una de las categorias para
+                      buscar.
+                    </p>
+                  </marquee>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
   </div>
-  <div class="row bg-danger p-4 fixed-bottom">
-    <audio controls>
-      <source :src="audio" type="audio/mpeg">
-    </audio>
-
-  </div>
 </template>
 
 <style>
+#app {
+  font-family: Quicksand, Avenir, Helvetica, Arial, sans-serif;
+}
+
 ul li a {
   color: #fff;
   text-decoration: none;
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,35 +1,121 @@
 <template>
-    <div class="card" style="width: 10rem;">
-        <img :src=" imge " class="card-img-top" :alt="titulo">
-        <div class="card-body">
-            <h5 class="card-title">{{ titulo }}</h5>
-            <p class="card-text"><small class="text-muted">{{ artista }}</small></p>
-        </div>
+  <a href="#" class="btn" @click="this.activo = 'si'" title="escuchar/ ver">
+    <div class="card" style="width: 10rem">
+      <img :src="imge" class="card-img-top" :alt="titulo" />
+      <div class="card-body text-start" style="height: 8rem">
+        <h5 class="card-title fs-6">{{ titulo }}</h5>
+        <p class="card-text">
+          <small class="text-muted">{{ artista }}</small>
+        </p>
+      </div>
     </div>
+  </a>
+  <!-- CANIONES -->
+  <div v-if="tipo === 'Canciones'">
+    <div
+      class="row p-4 fixed-bottom"
+      style="background-color: #eb5757"
+      v-show="this.activo != ''"
+    >
+      <div class="col-11">
+        <div class="row">
+          <audio controls>
+            <source :src="audio" type="audio/mpeg" />
+          </audio>
+        </div>
+      </div>
+      <div class="col-1 d-flex justify-content-center align-items-center">
+        <button type="button" @click="this.activo = ''" class="btn-close"></button>
+      </div>
+    </div>
+  </div>
+  <!-- ALBUMS -->
+  <div v-if="tipo === 'Álbums'">
+    <div
+      class="row p-3 fixed-bottom"
+      style="background-color: #eb5757"
+      v-show="this.activo != ''"
+    >
+      <div class="col-12 col-md-11">
+        <div class="row d-flex justify-content-center">
+          <div class="card" style="max-width: 540px">
+            <div class="row g-0">
+              <div class="col-md-4">
+                <img :src="imge" class="img-fluid rounded-start" :alt="titulo" />
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">{{ titulo }}</h5>
+                  <p class="card-text">
+                    Artista: {{ artista }}
+                    <br />
+                    Canciones: {{ tracks }}
+                  </p>
+                  <p class="card-text">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-md-1 mt-4 mt-md-0 d-flex justify-content-center align-items-center">
+        <button type="button" @click="this.activo = ''" class="btn-close"></button>
+      </div>
+    </div>
+  </div>
+  <!-- ARTISTAS -->
+  <div v-if="tipo === 'Artistas'">
+    <div
+      class="row p-3 fixed-bottom"
+      style="background-color: #eb5757"
+      v-show="this.activo != ''"
+    >
+      <div class="col-12 col-md-11">
+        <div class="row d-flex justify-content-center">
+          <div class="card" style="width: 18rem">
+            <img :src="imge" class="card-img-top mt-2" :alt="titulo" />
+            <div class="card-body">
+              <h5 class="card-title">{{ titulo }}</h5>
+              <p class="card-text">
+                Albunes compuestos: {{ albums }}
+                <br />
+                Número de fans: {{ fans }}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-md-1 d-flex justify-content-center align-items-center mt-4 mt-md-0">
+        <button type="button" @click="this.activo = ''" class="btn-close"></button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Card',
-    props: {
-        titulo: String,
-        artista: String,
-        imge: String,
-    }
-}
+  name: "Card",
+  data() {
+    return {
+      activo: "",
+    };
+  },
+  props: {
+    titulo: String,
+    artista: String,
+    imge: String,
+    audio: String,
+    tipo: String,
+    tracks: String,
+    albums: String,
+    fans: String,
+  },
+};
 </script>
-
-    <!-- <div class="card mb-3" style="max-width: 540px;">
-  <div class="row g-0">
-    <div class="col-md-4">
-      <img :src=" imge " class="img-fluid rounded-start" :alt="titulo">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">{{ titulo }}</h5>
-        <p class="card-text">{{ artista }}</p>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-      </div>
-    </div>
-  </div>
-</div> -->
+<style>
+q {
+  text-align: right;
+}
+</style>
